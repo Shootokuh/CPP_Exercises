97590443ef48128f66ddc7e0ce9d78cd1433d579
christian.ky <ky.christian77@gmail.com>
2023-02-15 09:23:19 +0100

    Saved changes

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -2,6 +2,33 @@
 
 #include <string>
 
+
 // A creature that is cute and can fight other ones.
-class Pokemon
-{};
+class Pokemon{
+    private:
+        std::string _name;
+        int _id;
+        static inline int _incremente = 0;
+
+    public:
+        Pokemon (const std::string& name): _name {name}, _id {_incremente++} {}
+
+        Pokemon (const Pokemon& other): _name{other.name}, _id{_incremente++} {}
+
+        Pokemon& operator=(const Pokemon& other){
+        if (this == &other){
+            _name = other._name;
+            }
+        return *this;
+        } 
+
+        const std::string &name() const{
+            return _name;
+        }
+
+        int id() const{
+            return _id;
+        }
+};
+
+
